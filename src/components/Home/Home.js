f96a6b88fb6e6f7a50b9a1cdeddc2695d0a31a2d
Rusyanto Rusanto <rusyanto@gmail.com>
2020-02-04 19:54:14 +0800

    Load data from domo dataset

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,11 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import domo from 'ryuu.js';
 import MaterialTable from 'material-table';
+// Material-UI core
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 // Material table icons
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
@@ -17,15 +23,32 @@ import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
 import Save from '@material-ui/icons/Save';
+import Backup from '@material-ui/icons/Backup';
+
+const useStyles = makeStyles(theme => ({
+  buttons: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+      marginLeft: theme.spacing(3),
+      marginRight: theme.spacing(1),
+    },
+    textAlign: 'right',
+  },
+  button: {
+    paddingRight: 8,
+  }
+}));
 
 function Home() {
+  const classes = useStyles();
+
   const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} fontSize="small" />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} fontSize="small" />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
     DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} fontSize="small" />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
     Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
     Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
     FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
@@ -33,14 +56,16 @@ function Home() {
     NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
     PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
     ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} fontSize="small" />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
     Save: forwardRef((props, ref) => <Save {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} fontSize="small" />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
     ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
     ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
   };
 
-  const [state, setState] = React.useState({
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [state, setState] = useState({
     columns: [
       { title: 'MPN', field: 'materialNo', editable: 'onUpdate' },
       { title: 'Description', field: 'description', editable: 'onUpdate' },
@@ -58,52 +83,75 @@ function Home() {
       { title: 'FY20W17', field: 'cw11', editable: 'onUpdate', type: 'numeric' },
       { title: 'FY20W18', field: 'cw12', editable: 'onUpdate', type: 'numeric' }
     ],
-    data: [
-      {
-        materialNo: '6008424', description: 'Cover - Timing',
-        cw0: 0, cw1: 0, cw2: 0, cw3: 0, cw4: 0, cw5: 0, cw6: 0, cw7: 0, cw8: 0, cw9: 0, cw10: 0, cw11: 0, cw12: 0
-      },
-      {
-        materialNo: '6004211', description: 'CM Shock Absorber: Rear,DAF SB220',
-        cw0: 0, cw1: 0, cw2: 0, cw3: 0, cw4: 0, cw5: 0, cw6: 0, cw7: 0, cw8: 0, cw9: 0, cw10: 0, cw11: 0, cw12: 0
-      },
-      {
-        materialNo: '7332121010', description: 'Glass-rr Or drop RH',
-        cw0: 0, cw1: 0, cw2: 0, cw3: 0, cw4: 0, cw5: 0, cw6: 0, cw7: 0, cw8: 0, cw9: 0, cw10: 0, cw11: 0, cw12: 0
-      },
-      {
-        materialNo: '6906043040', description: 'Lock Assy: Door R/L, for Toyota Taxi',
-        cw0: 0, cw1: 0, cw2: 0, cw3: 0, cw4: 0, cw5: 0, cw6: 0, cw7: 0, cw8: 0, cw9: 0, cw10: 0, cw11: 0, cw12: 0
-      },
-      {
-        materialNo: '6001581', description: 'Valve - Intake',
-        cw0: 0, cw1: 0, cw2: 0, cw3: 0, cw4: 0, cw5: 0, cw6: 0, cw7: 0, cw8: 0, cw9: 0, cw10: 0, cw11: 0, cw12: 0
-      },
-    ],
+    data: [],
   });
 
+  const handleSubmit = event => {
+    let contents = [];
+    let updateTime = new Date().toISOString();
+    state.data.forEach(record => {
+      record.updateTime = updateTime;
+      contents.push({ content: record });
+    });
+
+    setIsSubmitting(true);
+    domo.post(`/domo/datastores/v1/collections/smrtPlanner/documents/bulk`, contents)
+      .then(data => {
+        console.log(data);
+        setIsSubmitting(false);
+      })
+      .catch(e => console.log(e));
+  };
+
+  useEffect(() => {
+    domo.get(`/data/v1/forecastCalculated`)
+      .then(data => {
+        setState(prevState => {
+          return { ...prevState, data };
+        });
+        setIsLoading(false);
+      })
+      .catch(e => console.log(e));
+  }, []);
+
   return (
-    <MaterialTable
-      icons={tableIcons}
-      title="Forecast EOH"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowUpdate: (newData, oldData) =>
-          new Promise((resolve) => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                const index = data.indexOf(oldData);
-                data[index] = newData;
-                return { ...prevState, data };
-              });
-            }, 600)
-          }),
-      }}
-      style={{ padding: 8 }}
-    />
+    <Grid container>
+      <Grid item xs={12}>
+        <MaterialTable
+          icons={tableIcons}
+          title="Forecast EOH"
+          columns={state.columns}
+          data={state.data}
+          editable={{
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve();
+                  setState(prevState => {
+                    const data = [...prevState.data];
+                    const index = data.indexOf(oldData);
+                    data[index] = newData;
+                    return { ...prevState, data };
+                  });
+                }, 600)
+              }),
+          }}
+          style={{ padding: 8 }}
+          isLoading={isLoading}
+          options={{ pageSize: 10 }}
+        />
+      </Grid>
+      <Grid item xs={12} className={classes.buttons}>
+        {isSubmitting ? (
+          <CircularProgress style={{ padding: '0 40px' }} />
+        ) : (
+          <Button variant="contained" color="secondary" onClick={handleSubmit}>
+            <Backup className={classes.button} />Submit
+          </Button>
+      
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
